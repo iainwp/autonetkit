a90f2aadd108daa602d0c54bdac6e3cf33a24fd9
iainwp <I.W.Phillips@lboro.ac.uk>
2014-09-05 11:55:31 +0100

    added sub.py as algorithm for renaming interfaces in config files

---
+++ deter-files/sub.py
@@ -0,0 +1,29 @@
+import sys, re
+from addr import addr_to_name
+
+def main():
+  rv = addr_to_name()
+  # rv is a dict addr->ifname
+  inf_file = sys.argv[1]
+  map={}
+  with open(inf_file, "r") as f:
+    lines = f.read().splitlines()
+    for x in lines:
+      [a, i] = x.split(" ")
+      if a in rv:
+          map[i] = rv[a]
+
+  for x in sys.argv[2:]:
+    print "Processing file", x
+    with open(x, "r") as f:
+      contents = f.read()
+      for i in map:
+        contents = contents.replace(i, map[i])
+      f.close()
+    
+    with open(x, "w") as f:
+      f.write(contents)
+      f.close()
+
+if __name__ == '__main__':
+  main()
